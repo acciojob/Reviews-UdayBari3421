0ba593059c9e4f466a411d90b18aa45ca2e1a488
UdayBari3421 <udaybari2003@gmail.com>
2024-02-15 16:16:17 +0530

    submitting code-Thu Feb 15 2024 16:16:13 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -9,50 +9,45 @@ const App = () => {
       <h2 id="review-heading">Our Reviews</h2>
       <div className="review">
         <div className="author" id={"author-" + arr[next].id}>
-          <h3>
-            {/* <a href="#" id={"author-1" + arr[next].id}>
-              {arr[next].id}
-            </a> */}
-            {arr[next].name}
-          </h3>
-        </div>
-        <h4 className="job" id="job">
-          {arr[next].job}
-        </h4>
-        <p className="info" id="info">
-          {arr[next].text}
-        </p>
-        <div className="buttons">
+          <h2>{arr[next].name}</h2>
+          <h2 className="job" id="job">
+            {arr[next].job}
+          </h2>
+          <p className="info" id="info">
+            {arr[next].text}
+          </p>
+          <div className="buttons">
+            <button
+              className="prev-btn"
+              id="prev-btn"
+              onClick={() => (next === 0 ? setNext(0) : setNext(next - 1))}
+            >
+              Previous
+            </button>
+            <img
+              className="person-img"
+              id="person-img"
+              src={arr[next].image}
+              alt={arr[next].name}
+            />
+            <button
+              id="next-btn"
+              className="next-btn"
+              onClick={() =>
+                next === arr.length - 1 ? setNext(next) : setNext(next + 1)
+              }
+            >
+              Next
+            </button>
+          </div>
           <button
-            className="prev-btn"
-            id="prev-btn"
-            onClick={() => (next === 0 ? setNext(0) : setNext(next - 1))}
+            id="random-btn"
+            className="random-btn"
+            onClick={() => setNext(Math.floor(Math.random() * arr.length))}
           >
-            Previous
-          </button>
-          <img
-            className="person-img"
-            id="person-img"
-            src={arr[next].image}
-            alt={arr[next].name}
-          />
-          <button
-            id="next-btn"
-            className="next-btn"
-            onClick={() =>
-              next === arr.length - 1 ? setNext(next) : setNext(next + 1)
-            }
-          >
-            Next
+            surprise me
           </button>
         </div>
-        <button
-          id="random-btn"
-          className="random-btn"
-          onClick={() => setNext(Math.floor(Math.random() * arr.length))}
-        >
-          surprise me
-        </button>
       </div>
     </div>
   );
