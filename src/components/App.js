dfe6b732c68909743d1ec0df1ad98bf8a636b21c
UdayBari3421 <udaybari2003@gmail.com>
2024-02-15 13:11:09 +0530

    2.0

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,14 @@ const App = () => {
     <div className="review container">
       <h2 id="review-heading">Our Reviews</h2>
       <div className="review">
-        <h3 id="author">{arr[next].name}</h3>
+        <div className="author">
+          <h3 id={"author-" + arr[next].id}>{arr[next].name}</h3>
+        </div>
         <h4 id="job">{arr[next].job}</h4>
         <p id="info">{arr[next].text}</p>
         <div className="buttons">
           <button
+            className="prev-btn"
             id="prev-btn"
             onClick={() => (next === 0 ? setNext(0) : setNext(next - 1))}
           >
@@ -21,6 +24,7 @@ const App = () => {
           <img id="person-img" src={arr[next].image} alt={arr[next].name} />
           <button
             id="next-btn"
+            className="next-btn"
             onClick={() =>
               next === arr.length - 1 ? setNext(next) : setNext(next + 1)
             }
