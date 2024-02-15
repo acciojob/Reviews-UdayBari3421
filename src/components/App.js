eae96d902cafa10d3a7cdea97e125f2dbeec3ab6
UdayBari3421 <udaybari2003@gmail.com>
2024-02-15 17:23:49 +0530

    submitting code-Thu Feb 15 2024 17:23:45 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,53 +1,61 @@
 import React, { useState } from "react";
-import arr from "./Data";
-import "../styles/App.css";
+import data from "./Data";
+
 const App = () => {
-  const [next, setNext] = useState(0);
+  let [index, setIndex] = useState(0);
+
+  function handlePreviousClick() {
+    if (index == 0) {
+      setIndex(3);
+    } else {
+      setIndex(index - 1);
+    }
+  }
+
+  function hangleNextClick() {
+    if (index == 3) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  }
+
+  function handleRandomClick() {
+    let randomIndex;
+
+    do {
+      randomIndex = Math.floor(Math.random() * 4); // Generate a random number between 0 and 3
+    } while (randomIndex === index); // Loop until the random index is different from the current value
+
+    setIndex(randomIndex);
+    console.log(randomIndex);
+  }
 
   return (
-    <div className="review container">
-      <h2 id="review-heading">Our Reviews</h2>
+    <div>
+      <h1 id="review-heading">Our Reviews</h1>
       <div className="review">
-        <div className="author">
-          <h3 id={"author-" + (next + 1)}>{arr[next].name}</h3>
+        <div>
+          <img className="person-img" src={data[index].image}></img>
         </div>
-        <h4 className="job" id="job">
-          {arr[next].job}
-        </h4>
-        <p className="info" id="info">
-          {arr[next].text}
-        </p>
-        <div className="buttons">
-          <button
-            className="prev-btn"
-            id="prev-btn"
-            onClick={() => (next === 0 ? setNext(0) : setNext(next - 1))}
-          >
+        <div>
+          <span className="author" id={`author-${index + 1}`}>
+            Name: {data[index].name}
+          </span>
+          <p className="job">Job: {data[index].job}</p>
+          <p className="info">Text: {data[index].text}</p>
+        </div>
+        <div>
+          <button className="prev-btn" onClick={handlePreviousClick}>
             Previous
           </button>
-          <img
-            className="person-img"
-            id="person-img"
-            src={arr[next].image}
-            alt={arr[next].name}
-          />
-          <button
-            id="next-btn"
-            className="next-btn"
-            onClick={() =>
-              next === arr.length - 1 ? setNext(next) : setNext(next + 1)
-            }
-          >
+          <button className="next-btn" onClick={hangleNextClick}>
             Next
           </button>
+          <button className="random-btn" onClick={handleRandomClick}>
+            surprise me
+          </button>
         </div>
-        <button
-          id="random-btn"
-          className="random-btn"
-          onClick={() => setNext(Math.floor(Math.random() * arr.length))}
-        >
-          surprise me
-        </button>
       </div>
     </div>
   );
